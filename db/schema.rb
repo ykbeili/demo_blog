822c8cb4f41223ab5ec7e9e7c22b3bcef5dbef37
Yousef Kbeili <ykbeili@gmail.com>
2019-11-14 20:17:14 -0600

    Add comments controller and comment model and run db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_14_192213) do
+ActiveRecord::Schema.define(version: 2019_11_15_021613) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
