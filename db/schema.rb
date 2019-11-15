066005e46f71eaaf2dce331ac40e9da6c149278f
Yousef Kbeili <ykbeili@gmail.com>
2019-11-14 20:55:30 -0600

    Add create comments and deleting comments for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_15_021613) do
+ActiveRecord::Schema.define(version: 2019_11_15_022025) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "comment"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
